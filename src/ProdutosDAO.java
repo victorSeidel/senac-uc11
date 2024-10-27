3892b818484ffee402b9d4ec342335321c34d30d
Victor Seidel <victorgabrielms88@gmail.com>
2024-10-27 12:05:20 -0300

    func register products working

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -8,10 +8,12 @@
  * @author Adm
  */
 
+import java.awt.HeadlessException;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -23,11 +25,18 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try {
+            conn = new conectaDAO().connectDB();
+            String sql = "INSERT INTO produtos (nome, valor) VALUES (?, ?)";
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setDouble(2, produto.getValor());
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+        } 
+        catch (HeadlessException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + e.getMessage());
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
